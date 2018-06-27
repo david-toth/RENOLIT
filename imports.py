b183348fcb818970716757494fca926b1dc4ac8a
Toth <usftdt0@renolit.net>
2018-06-27 14:06:55 -0500

    Initial steps

---
+++ imports.py
@@ -0,0 +1,10 @@
+import os
+import time
+import datetime as dt
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn as sk
+from tkinter import *
+print("Imports successful")
+# tk._test()
