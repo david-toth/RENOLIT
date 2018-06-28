2ef2ab7d53b45f65e640529a526923e263d13aaa
d-toth1 <dtoth1@nd.edu>
2018-06-28 10:01:36 -0500

    Testing features

--- gui.py
+++ gui.py
@@ -1,22 +1,34 @@
 from imports import *
 
-class MyFirstGUI:
+class RenolitGUI:
     def __init__(self, master):
         self.master = master
-        master.title("A simple GUI")
+        master.title("Renolit Quality Control")
 
-        self.label = Label(master, text="This is our first GUI!")
+        self.label = Label(master, text="Quality Control Data Project")
         self.label.pack()
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
+        self.calc_button = Button(master, text="Calculation", command=self.calculate)
+        self.calc_button.pack()
 
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.pack()
 
-    def greet(self):
-        print("Greetings!")
+    def calculate(self):
+        x = []
+        for i in range(100):
+            x.append(i)
+        y = []
+        for i in range(100):
+            y.append(i**2)
+        np_y = np.array(y)
+        np_x = np.array(x)
+        plt.scatter(np_x, np_y)
+        plt.show()
+    def close(self):
+        plt.close()
 
 root = Tk()
-my_gui = MyFirstGUI(root)
+my_gui = RenolitGUI(root)
+root.geometry("500x500")
 root.mainloop()
