3fc05e54472fa6a79e2f1336ed4c28052e6a605c
d-toth1 <dtoth1@nd.edu>
2018-07-12 10:34:06 -0500

    Rename gui -- QCAnalysis; Add function to CombineData; clean-up

--- CombineData.py
+++ CombineData.py
@@ -29,4 +29,40 @@ def Combine_Data():
 			print(all_data)
 			all_data.to_csv('CombinedData.csv')
 
-Combine_Data()
+def RemoveCharacters():
+	'''
+	Function that cleans up mis-copied characteristic names in the database.
+	Primarily interested in degree symbol (°) errors.
+	'''
+
+	database_path = "C:/Users/usftdt0/Documents/CleanDataBase.xlsx"
+	database_xlsx = pd.ExcelFile(database_path)
+	database_df = pd.read_excel(database_xlsx)
+	characteristics_list = database_df['Charac.'].tolist()
+
+	for n, i in enumerate(characteristics_list):
+		if i == 'GLOSS 20Ã‚Â° drive side' or i == 'GlOSS 20Ã‚Â° drive side':
+			characteristics_list[n] = 'Gloss 20° drive side'
+		if i == "GLOSS 20Ã‚Â° heat side" or i == "GlOSS 20Ã‚Â° heat side":
+			characteristics_list[n] = "Gloss 20° heat side"
+		if i == 'GLOSS 60Ã‚Â° drive side' or i == 'GlOSS 60Ã‚Â° drive side':
+			characteristics_list[n] = "Gloss 60° drive side"
+		if i == 'GLOSS 60Ã‚Â° heat side' or i == 'GlOSS 60Ã‚Â° heat side':
+			characteristics_list[n] = "Gloss 60° heat side"
+		if i == 'GLOSS 60Ã‚Â°  lacquer drive side' or i == 'GLOSS 60Ã‚Â°  lacquer drive side':
+			characteristics_list[n] = "Gloss 60° lacquer drive side"
+		if i == 'GlOSS 85Ã‚Â° drive side' or i == 'GLOSS 85Ã‚Â° drive side':
+			characteristics_list[n] = "Gloss 85° drive side"
+		if i == "Shrink(10'/70Ã‚Â°) drive side":
+			characteristics_list[n] = "Shrink (10'/70°) drive side"
+		if i == "Shrink(10'/80Ã‚Â°) drive side" or i == "Shrink (10'/80Ã‚Â°) drive side":
+			characteristics_list[n] = "Shrink (10'/80°) drive side"
+		if i == "Shrink(10'/100Ã‚Â°) drive side":
+			characteristics_list[n] = "Shrink (10'/100°) drive side"
+
+	characteristics_list = sorted(characteristics_list)
+	new_df = pd.DataFrame({'Charac.': characteristics_list})
+	new_df.to_excel('ModCharac.xlsx')
+
+# Combine_Data()
+RemoveCharacters()
