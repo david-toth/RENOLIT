ba43bf061e0f3982020bd8be17cdeccc6dacdcf2
d-toth1 <dtoth1@nd.edu>
2018-08-03 09:27:38 -0500

    Deletion of setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(name='QCAnalysis',
-		version='0.1',
-		author='David Toth',
-	ext_modules=cythonize("QCAnalysis.pyx")
-)
