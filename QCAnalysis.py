483c9b748d17291e34784debb0902ff006ab2fb0
d-toth1 <dtoth1@nd.edu>
2018-08-06 14:51:49 -0500

    Final updates for summer

--- QCAnalysis.py
+++ QCAnalysis.py
@@ -30,36 +30,6 @@ class RenolitGUI:
 		self.label.configure(font=('Arial', 18, 'bold'))
 		self.label.grid(column=1, row=0, sticky=(N),  padx=5, pady=5)
 
-		# self.customer = StringVar()
-		# self.customer.set(filter_customerlist[0])
-		# self.customer_label = Label(root, text="Customers")
-		# self.customer_label.configure(font=('Arial', 10, 'bold'))
-		# self.customer_label.grid(column=1, row=1, sticky=(S,W), padx = (10,0))
-		# self.customer_menu = Listbox(root,width = 45,selectmode=EXTENDED, takefocus=0)
-		# for i in filter_customerlist:
-		# 	self.customer_menu.insert(END, i)
-		# self.customer_menu.bind("<<ListboxSelect>>", self.DoubleClick)
-		# self.customer_menu.bind("<Button-3>", self.RemoveEntry)
-		# self.customer_menu.grid(column=1, row=2, sticky=(N,W), padx = (10,0))
-		# self.scrollbar = Scrollbar(root, orient=VERTICAL, command=self.customer_menu.yview)
-		# self.scrollbar.grid(column=2, row=2, sticky='ns')
-		# self.customer_menu.configure(yscrollcommand = self.scrollbar.set)
-
-		# self.article = StringVar()
-		# self.article.set(article_list[0])
-		# self.article_label = Label(root, text="Article Number")
-		# self.article_label.configure(font=('Arial', 10, 'bold'))
-		# self.article_label.grid(column=1, row=3, sticky=(N,W), padx = (10,0))
-		# self.article_option_menu = Listbox(root, width=45, selectmode=EXTENDED, takefocus=0)
-		# for i in article_list:
-		# 	self.article_option_menu.insert(END, i)
-		# self.article_option_menu.bind("<<ListboxSelect>>", self.DoubleClick)
-		# self.article_option_menu.bind("<Button-3>", self.RemoveEntry)
-		# self.article_option_menu.grid(column=1, row=4, sticky=(N,W), padx = (10,0), pady=(0,10))
-		# self.scrollbar2 = Scrollbar(root, orient=VERTICAL, command=self.article_option_menu.yview)
-		# self.scrollbar2.grid(column=2, row=4, sticky='ns')
-		# self.article_option_menu.configure(yscrollcommand = self.scrollbar2.set)
-
 		self.characteristics = StringVar()
 		self.characteristics.set(optionlist[0])
 		self.characteristics_label = Label(root, text="QC Characteristics")
@@ -74,17 +44,6 @@ class RenolitGUI:
 		self.scrollbar3 = Scrollbar(root, orient=VERTICAL, command=self.characteristics_option_menu.yview)
 		self.scrollbar3.grid(column=2, row=4, sticky='ns')
 		self.characteristics_option_menu.configure(yscrollcommand = self.scrollbar3.set)
-
-		# self.check_label = Label(root, text="Select filter(s):")
-		# self.check_label.grid(column=4, row=1)
-		#
-		# self.var1 = IntVar()
-		# self.checkbox_cust = Checkbutton(root, text='Customer', variable=self.var1)
-		# self.checkbox_cust.grid(column=5, row=1, padx=5)
-		#
-		# self.var2 = IntVar()
-		# self.checkbox_article = Checkbutton(root, text='Article No.', variable=self.var2)
-		# self.checkbox_article.grid(column=6, row=1,padx=5)
 
 		self.DateLabel = Label(root, text='Date range (YYYY-MM-DD):')
 		self.DateLabel.grid(column=4, row=1, padx=(0,10))
@@ -124,6 +83,10 @@ class RenolitGUI:
 		self.trend = Button(root, text="Show Trend Line", command=self.TrendLine)
 		self.trend.configure(width=15)
 		self.trend.grid(column=5, row=4,  padx=10)
+
+		self.Update = Button(root, text='Update Database', command=self.UpdateDatabase)
+		self.Update.configure(width=15)
+		self.Update.grid(column=6, row=4, padx=10)
 
 		self.quit_button = Button(root, text='Quit', command=self.Quit)
 		self.quit_button.configure(width=15)
@@ -255,82 +218,53 @@ class RenolitGUI:
 		figs = list(map(plt.figure, plt.get_fignums()))
 		for i in figs:
 			plt.close(i)
-		df = pd.read_excel(database_path, "QC Data")
+		t0 = time.time()
 		raw_data = gl.glob("O:/QUALITY/QC Data Project/Raw Data/*")
 		df_rawdata = pd.DataFrame()
+		last_update = '2018-07-02 10:52:12'
+		#### Only read files that are new
+		print("Sorting through raw data files...")
+		try:
+			for file in tqdm(raw_data):
+			    stamp = dt.datetime.fromtimestamp(os.path.getctime(file)).strftime("%Y-%m-%d %H:%M:%S")
+			    if stamp > last_update:
+			        temp_df = pd.read_excel(file)
+			        df_rawdata = df_rawdata.append(temp_df, ignore_index=True)
+			df_rawdata = df_rawdata.drop_duplicates()
 
-		for file in tqdm(raw_data):
-			temp_df = pd.read_excel(file)
-			df_rawdata = df_rawdata.append(temp_df, ignore_index=True)
-		df_rawdata = df_rawdata.drop_duplicates()
+			year_list = []
+			for i in range(len(df_rawdata.index)):
+			    i = 2018
+			    year_list.append(i)
 
-		if (1):
-			raw_order, raw_charac, raw_poid, raw_rollno = df_rawdata['Order:'].tolist(), df_rawdata['Charac.'].tolist(), df_rawdata['PO ID'].tolist(), df_rawdata['PO lfn'].tolist()
-			order, charac, po_id, roll_no = df["Order:"].tolist(), df['Charac.'].tolist(), df['PO ID'].tolist(), df['PO lfn'].tolist()
+			index = df_rawdata.columns.get_loc('Date create')
+			new_index = index + 1
 
-			for a,b,c,d in tqdm(zip(raw_order, raw_charac, raw_poid, raw_rollno)):
-				for e,f,g,h in zip(order, charac, po_id, roll_no):
-					if a==e and b==f and c==g and d == h:
-						row = df_rawdata.loc[(df_rawdata["Order:"] == a) & (df_rawdata["Charac."] == b) & (df_rawdata["PO ID"] == c) & (df_rawdata["PO lfn"] == d)]
-						index = row.index
-						df_rawdata = df_rawdata.drop(index)
+			df_rawdata.insert(loc=new_index, column='Year', value=year_list)
 
-		year_list = []
-		for i in range(len(df_rawdata.index)):
-			i = 2018
-			year_list.append(i)
+			dates = df_rawdata['Date create']
+			dates = dates.str[:5]
+			dates = dates.str.replace(".", "-")
 
-		index = df_rawdata.columns.get_loc('Date create')
-		new_index = index + 1
+			years = df_rawdata['Year']
+			new_dates = dates.astype(str) + '-' + years.astype(str)
+			day = new_dates.str[0:2]
+			month = new_dates.str[3:5]
+			year = new_dates.str[6:]
 
-		df_rawdata.insert(loc=new_index, column='Year', value=year_list)
+			new_dates = year.astype(str) + '-' + month.astype(str) + '-' + day.astype(str)
+			df_rawdata['New Dates'] = new_dates
+			datetime = pd.to_datetime(df_rawdata['New Dates'])
+			date_list = datetime.tolist()
+			df_rawdata = df_rawdata.drop(columns=['New Dates'])
+			df_rawdata.insert(loc=(index-1), column='Dates', value=date_list)
 
-		dates = df_rawdata['Date create']
-		dates = dates.str[:5]
-		dates = dates.str.replace(".", "-")
-
-		years = df_rawdata['Year']
-		new_dates = dates.astype(str) + '-' + years.astype(str)
-		day = new_dates.str[0:2]
-		month = new_dates.str[3:5]
-		year = new_dates.str[6:]
-
-		new_dates = year.astype(str) + '-' + month.astype(str) + '-' + day.astype(str)
-		df_rawdata['New Dates'] = new_dates
-		datetime = pd.to_datetime(df_rawdata['New Dates'])
-		date_list = datetime.tolist()
-		df_rawdata = df_rawdata.drop(columns=['New Dates'])
-		df_rawdata.insert(loc=(index-1), column='Dates', value=date_list)
-
-		df_rawdata.to_excel('NewDataBase.xlsx')
-		database_df = pd.read_excel('NewDataBase.xlsx')
-		characteristics_list = database_df['Charac.'].tolist()
-
-		for n, i in enumerate(characteristics_list):
-			if i == 'GLOSS 20Ã‚Â° drive side' or i == 'GlOSS 20Ã‚Â° drive side':
-				characteristics_list[n] = 'Gloss 20° drive side'
-			if i == "GLOSS 20Ã‚Â° heat side" or i == "GlOSS 20Ã‚Â° heat side":
-				characteristics_list[n] = "Gloss 20° heat side"
-			if i == 'GLOSS 60Ã‚Â° drive side' or i == 'GlOSS 60Ã‚Â° drive side':
-				characteristics_list[n] = "Gloss 60° drive side"
-			if i == 'GLOSS 60Ã‚Â° heat side' or i == 'GlOSS 60Ã‚Â° heat side':
-				characteristics_list[n] = "Gloss 60° heat side"
-			if i == 'GLOSS 60Ã‚Â°  lacquer drive side' or i == 'GLOSS 60Ã‚Â°  lacquer drive side':
-				characteristics_list[n] = "Gloss 60° lacquer drive side"
-			if i == 'GlOSS 85Ã‚Â° drive side' or i == 'GLOSS 85Ã‚Â° drive side':
-				characteristics_list[n] = "Gloss 85° drive side"
-			if i == "Shrink(10'/70Ã‚Â°) drive side":
-				characteristics_list[n] = "Shrink (10'/70°) drive side"
-			if i == "Shrink(10'/80Ã‚Â°) drive side" or i == "Shrink (10'/80Ã‚Â°) drive side":
-				characteristics_list[n] = "Shrink (10'/80°) drive side"
-			if i == "Shrink(10'/100Ã‚Â°) drive side":
-				characteristics_list[n] = "Shrink (10'/100°) drive side"
-
-		characteristics_list = sorted(characteristics_list)
-		database_df = database_df.drop(columns=['Charac.'])
-		charac_index = database_df.columns.get_loc('Plan No.')
-		database_df.insert(loc=(charac_index-1), column='Charac.', value=characteristics_list)
-		CleanDataBase_df = pd.concat([df, database_df])
+			df_rawdata.to_excel('NewDataBase.xlsx')
+			t1 = time.time()
+			print("Update time:", float(t1-t0)/60, "minutes")
+		except:
+			print("No new data files found.")
+			pass
 
 	def Quit(self):
 		figs = list(map(plt.figure, plt.get_fignums()))
@@ -359,3 +293,68 @@ root.columnconfigure(5, weight=1)
 root.columnconfigure(6, weight=1)
 my_gui = RenolitGUI(root)
 root.mainloop()
+
+#################################
+def Combine_Data():
+	"""
+	Function that collects several Excel files and concatonates
+	them into one large file. Use the first 'if' block you want to write
+	directly to an Excel file -- WARNING: VERY SLOW. However, to save time,
+	use the second 'if' block, which writes to a CSV file instead.
+	"""
+
+	if (0):
+		raw_data = gl.glob("O:/QUALITY/QC Data Project/Raw Data/*")
+		print(raw_data)
+		excel_file = ExcelWriter('CombinedData.xlsx')
+		all_data = pd.DataFrame()
+		for i in raw_data:
+			df = pd.read_excel(i)
+			all_data = all_data.append(df, ignore_index=True)
+			print(all_data)
+			all_data.to_excel(excel_file)
+		excel_file.save()
+	if (1):
+		raw_data = gl.glob("O:/QUALITY/QC Data Project/Raw Data/*")
+		print(raw_data)
+		all_data = pd.DataFrame()
+		for i in raw_data:
+			df = pd.read_excel(i)
+			all_data = all_data.append(df, ignore_index=True)
+			print(all_data)
+			all_data.to_csv('CombinedData.csv')
+
+def RemoveCharacters():
+	'''
+	Function that cleans up mis-copied characteristic names in the database.
+	Primarily interested in degree symbol (°) errors.
+	'''
+
+	database_path = "C:/Users/usftdt0/Documents/CleanDataBase.xlsx"
+	database_xlsx = pd.ExcelFile(database_path)
+	database_df = pd.read_excel(database_xlsx)
+	characteristics_list = database_df['Charac.'].tolist()
+
+	for n, i in enumerate(characteristics_list):
+		if i == 'GLOSS 20Ã‚Â° drive side' or i == 'GlOSS 20Ã‚Â° drive side':
+			characteristics_list[n] = 'Gloss 20° drive side'
+		if i == "GLOSS 20Ã‚Â° heat side" or i == "GlOSS 20Ã‚Â° heat side":
+			characteristics_list[n] = "Gloss 20° heat side"
+		if i == 'GLOSS 60Ã‚Â° drive side' or i == 'GlOSS 60Ã‚Â° drive side':
+			characteristics_list[n] = "Gloss 60° drive side"
+		if i == 'GLOSS 60Ã‚Â° heat side' or i == 'GlOSS 60Ã‚Â° heat side':
+			characteristics_list[n] = "Gloss 60° heat side"
+		if i == 'GLOSS 60Ã‚Â°  lacquer drive side' or i == 'GLOSS 60Ã‚Â°  lacquer drive side':
+			characteristics_list[n] = "Gloss 60° lacquer drive side"
+		if i == 'GlOSS 85Ã‚Â° drive side' or i == 'GLOSS 85Ã‚Â° drive side':
+			characteristics_list[n] = "Gloss 85° drive side"
+		if i == "Shrink(10'/70Ã‚Â°) drive side":
+			characteristics_list[n] = "Shrink (10'/70°) drive side"
+		if i == "Shrink(10'/80Ã‚Â°) drive side" or i == "Shrink (10'/80Ã‚Â°) drive side":
+			characteristics_list[n] = "Shrink (10'/80°) drive side"
+		if i == "Shrink(10'/100Ã‚Â°) drive side":
+			characteristics_list[n] = "Shrink (10'/100°) drive side"
+
+	characteristics_list = sorted(characteristics_list)
+	new_df = pd.DataFrame({'Charac.': characteristics_list})
+	new_df.to_excel('ModCharac.xlsx')
--- imports.py
+++ imports.py
@@ -9,23 +9,11 @@ import datetime as dt
 import glob as gl
 import pandas as pd
 from pandas import ExcelWriter
-# import pandas_datareader as pdr
 from openpyxl import load_workbook
 import numpy as np
-# import plotly as py
-# py.tools.set_credentials_file(username='dtoth1', api_key='tsyRiH7QSz6JEigl7xLf')
-# import plotly.graph_objs as go
 import matplotlib.pyplot as plt
 from matplotlib.dates import (MONTHLY, DateFormatter, rrulewrapper, RRuleLocator, YearLocator, MonthLocator, DayLocator)
 from matplotlib.font_manager import FontProperties 
-import sklearn as sk
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import *
-from sklearn.linear_model import *
-from sklearn.svm import *
-from sklearn.metrics import *
-from sklearn.grid_search import *
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import messagebox
